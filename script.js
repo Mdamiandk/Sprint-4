34a8455afb8fa1370787308290c00233b29b58bc
DamianK <115102203+Mdamiandk@users.noreply.github.com>
2024-08-26 23:03:00 +0200

    script.js

--- script.js
+++ script.js
@@ -2,60 +2,71 @@ const addButton = document.getElementById("addButton");
 const inputArea = document.getElementById("task");
 const taskList = document.getElementById("taskList");
 
-addButton.addEventListener("click", function () {
-	const taskText = inputArea.value.trim();
-
-	if (inputArea.value === "") {
+function validateText(text) {
+	const trimmedText = text.trim();
+	if (!trimmedText) {
 		alert("Nazwa zadania nie moze byc pusta");
+		return null;
+	}
+	return trimmedText;
+}
+
+function editButtonHandler(button) {
+	const buttonText = button.target.innerText;
+	const li = button.target.parentNode;
+	if (buttonText === "Edytuj") {
+		const text = li.firstChild.textContent;
+		li.firstChild.textContent = "";
+		const editInputText = document.createElement("input");
+		editInputText.value = "";
+		li.prepend(editInputText);
+		button.target.innerText = "Zapisz zmiany";
+	} else {
+		// Zapisz zmiany
+		const text = validateText(li.firstChild.value);
+		if (!text) {
+			return;
+		}
+
+		const textTrimmed = text;
+		li.removeChild(li.firstChild);
+		li.prepend(textTrimmed);
+		button.target.innerText = "Edytuj";
+	}
+}
+
+function removeButtonHandler(button) {
+	const li = button.target.parentNode;
+	taskList.removeChild(li);
+}
+
+function addTask() {
+	const taskText = validateText(inputText.value);
+	if (!taskText) {
 		return;
 	}
 
-	const newLi = document.createElement("li");
-	const liSpan = document.createElement("span");
-	liSpan.textContent = taskText;
+	const newTask = document.createElement("li");
+	newTask.textContent = taskText;
 
+	const editButton = createButton("Edytuj");
+	editButton.addEventListener("click", editButtonHandler);
+
+	const removeButton = createButton("Usun");
+	removeButton.addEventListener("click", removeButtonHandler);
+
+	newTask.appendChild(editButton);
+	newTask.appendChild(removeButton);
+	taskList.appendChild(newTask);
+
+  inputText.value = '';
+}
+
+function createButton(text) {
 	const editButton = document.createElement("button");
-	editButton.textContent = "Edytuj";
-	const deleteButton = document.createElement("button");
-	deleteButton.textContent = "Usun";
+	editButton.textContent = text;
 
-	newLi.appendChild(liSpan);
-	newLi.appendChild(editButton);
-	newLi.appendChild(deleteButton);
+	return editButton;
+}
 
-	taskList.appendChild(newLi);
-
-	deleteButton.addEventListener("click", function () {
-		taskList.removeChild(newLi);
-	});
-
-	editButton.addEventListener("click", function () {
-		const newButton = document.createElement("button");
-		const newInputArea = document.createElement("input");
-
-		newInputArea.type = "text";
-		newInputArea.value = liSpan.textContent;
-
-		newLi.removeChild(editButton);
-		newLi.removeChild(liSpan);
-		newButton.textContent = "Zatwierdz zmiany";
-		newLi.insertBefore(newButton, deleteButton);
-		newLi.prepend(newInputArea);
-
-		newButton.addEventListener("click", function () {
-			const editedText = newInputArea.value.trim();
-
-			if (newInputArea.value === "") {
-				alert("Musisz cos wprowadzic");
-				return;
-			}
-
-			liSpan.textContent = editedText;
-			newLi.insertBefore(liSpan, newInputArea);
-			newLi.insertBefore(editButton, deleteButton);
-			newLi.removeChild(newButton);
-			newLi.removeChild(newInputArea);
-		});
-	});
-	inputArea.value = "";
-});
+addButton.addEventListener("click", addTask);
